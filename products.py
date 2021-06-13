ff1a21120bef458ee0765b525c676141f95be517
Olinto <olintojose@gmail.com>
2021-06-12 20:55:09 -0400

    first commit

---
+++ products.py
@@ -0,0 +1,7 @@
+products = [
+    {"name": "laptop", "price": 1200, "quantity":4},
+    {"name": "PS5", "price": 400, "quantity":7},
+    {"name": "IPAD", "price": 500, "quantity":5},
+    {"name": "Iphone", "price": 800, "quantity":3},
+
+]
